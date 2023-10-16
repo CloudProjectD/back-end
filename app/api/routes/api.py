7f199bf2a136aa4ac5d2a32d27270491bd00399f
Eeap <kdg97811@naver.com>
2023-10-17 00:28:20 +0900

    feat: add create market post route & method

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.routes import users
+from app.api.routes import rooms
 
 
 router = APIRouter()
 router.include_router(users.router, tags=["users"], prefix="/user")
+router.include_router(rooms.router, tags=["rooms"])
---
+++ app/api/routes/rooms.py
@@ -0,0 +1,31 @@
+import logging
+from typing import Any
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.api.dependencies import database
+from app.models.domain import rooms
+from app.crud import crud_rooms, crud_posts
+
+router = APIRouter()
+
+
+@router.post("/create/room")
+def create_room_posts(*, db: Session = Depends(database.get_db), room_in: rooms.RoomCreate) -> Any:
+  """
+  Create new user.
+  """
+  # post data create
+  post_data = crud_posts.create(db=db, obj_in=room_in)
+  print(post_data)
+  # room data create
+  room_data = crud_rooms.create(db=db, obj_in=room_in, post_id=post_data.id)
+  print(room_data)
+  if room_data and post_data:
+    return {
+      "message": "create success"
+    }
+  else:
+    raise HTTPException(
+      status_code=500,
+      detail="create failed",
+    )
