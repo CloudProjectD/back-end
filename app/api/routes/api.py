302219cf18d6e48c432389ee33790e3fd66883ef
dusdjhyeon <dusdj0813@gmail.com>
2023-11-19 16:59:16 +0900

    feat: add oauth routes

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 
 from app.api.routes import users
 from app.api.routes import markets
-
+from app.api.routes import oauth
 
 router = APIRouter()
 router.include_router(users.router, tags=["users"], prefix="/user")
 router.include_router(markets.router, tags=["markets"], prefix="/market")
+router.include_router(oauth.router, tags=["auth"], prefix="/auth")
