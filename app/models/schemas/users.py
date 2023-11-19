6e516155bacbb3134fa0df58e0313ae5179f3b45
dusdjhyeon <dusdj0813@gmail.com>
2023-11-19 17:01:21 +0900

    feat: modify a user that already exists

--- app/models/schemas/users.py
+++ app/models/schemas/users.py
@@ -5,10 +5,13 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .posts import Post  # noqa: F401
+    from .likes import Like  # noqa: F401
 
 
 class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    username = Column(String(255), index=True)
+    username = Column(String(255), index=True, nullable=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
+    major = Column(String(255), index=True, nullable=True)
     posts = relationship("Post", back_populates="user")
+    likes = relationship("Like", back_populates="user")
