de315a96c2c2d213b267b04d10eeba35c8de13a9
dusdjhyeon <dusdj0813@gmail.com>
2023-11-23 14:40:40 +0900

    feat: add login respone attr

--- app/services/google.py
+++ app/services/google.py
@@ -1,9 +1,10 @@
 from typing import Any, Optional
 import requests
-from fastapi import HTTPException, status, Depends
+import json
+from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse
 from fastapi_users.authentication import AuthenticationBackend
 from fastapi_users.authentication.strategy import Strategy
-from sqlalchemy.orm import Session
 
 from app.services.user_manager import bearer_transport, get_jwt_strategy
 from app.db.fastapi_user import User
@@ -14,6 +15,7 @@ from app.crud.crud_users import create_user, get_user_by_email
 class GoogleAuthBackend(AuthenticationBackend):
     async def login(self, strategy: Strategy, user: User) -> Any:
         strategy_response = await super().login(strategy, user)
+        member = True
         for account in user.oauth_accounts:
             usermail = account.account_email
 
@@ -28,10 +30,11 @@ class GoogleAuthBackend(AuthenticationBackend):
                 user = get_user_by_email(db=db, email=usermail)
                 if not user:
                     create_user(db=db, email=usermail)
+                    member = False
             finally:
                 db.close()
 
-        return strategy_response
+        return JSONResponse(content={"info": json.loads(strategy_response.body), "member": member})
 
     def get_google_access_token(self, user: User) -> Optional[str]:
         for account in user.oauth_accounts:
