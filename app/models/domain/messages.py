774619c5825981f68c93e5b834dd04a9e6cbc410
dusdjhyeon <dusdj0813@gmail.com>
2023-11-30 00:20:06 +0900

    feat: add MessageInput

--- app/models/domain/messages.py
+++ app/models/domain/messages.py
@@ -1,11 +1,20 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
+import uuid
+
 
 class Message(BaseModel):
+    id: str = str(uuid.uuid4())
     post_id: int
     category: str
-    sender_id: Optional[int] = None
-    receipient_id: int
+    sender: Optional[int] = None
+    recipient: int
     content: str
-    created_at: Optional[datetime] = None
+    created_at: datetime = datetime.today()
+
+class MessageInput(BaseModel):
+    post_id: int
+    category: str
+    recipient: int
+    content: str
