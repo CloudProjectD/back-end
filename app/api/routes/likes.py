55cfb96f95de3d428f92396c733e175ae0a10c0f
nana <knh4769@khu.ac.kr>
2023-12-01 18:32:00 +0900

    feat: add user's like item list route

--- app/api/routes/likes.py
+++ app/api/routes/likes.py
@@ -1,16 +1,39 @@
-from typing import Any
+from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.services.aws import s3_get
 from app.api.dependencies import database
-from app.crud.crud_likes import create_like, delete_like
-from app.models.domain import users
+from app.crud.crud_likes import create_like, delete_like, get_liked_post_by_user
+from app.models.domain.likes import LikePostResponse
 from app.services.user_manager import current_active_user
 from app.crud.crud_users import get_user_by_email
 from app.db.fastapi_user import User
 
 
 router = APIRouter()
+
+
+@router.get(
+    "/list",
+    response_model=List[LikePostResponse],
+    description="사용자가 관심 표시한 물품의 리스트를 반환합니다.",
+)
+def get_liked_posts(
+    *,
+    db: Session = Depends(database.get_db),
+    current_user: User = Depends(current_active_user),
+) -> Any:
+    current_user_id = get_user_by_email(db=db, email=current_user.email).id
+
+    liked_posts = get_liked_post_by_user(db=db, user_id=current_user_id)
+
+    if liked_posts:
+        return liked_posts
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail="No liked post found for this user",
+        )
 
 
 @router.post("/post/{post_id}", description="게시글에 관심 표시를 합니다.")
