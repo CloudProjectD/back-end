292e8edba42c17f9aac211d42d7dd13c57c2635e
Eeap <kdg97811@naver.com>
2023-12-01 20:24:37 +0900

    feat: add free data model

--- app/models/domain/frees.py
+++ app/models/domain/frees.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 import datetime
 from app.resources.status import Status
+from typing import List
 
 
 class FreeCreate(BaseModel):
@@ -10,3 +11,21 @@ class FreeCreate(BaseModel):
     content: str
     category: str
     status: Status
+
+
+class FreeGet(BaseModel):
+    post_id: Optional[int] = None
+    title: str
+    content: str
+    status: Status
+    category: str
+    user_id: int
+    created_at: datetime.datetime
+    image_list: List[str]
+
+
+class FreeUpdate(BaseModel):
+    title: str
+    content: str
+    status: Status
+    category: str
