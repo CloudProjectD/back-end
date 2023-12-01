204e5b07b5e6aa8d1f5e6c5e4e7f44876ab8d7ba
nana <knh4769@khu.ac.kr>
2023-12-01 16:09:45 +0900

    feat: add like delete  route

--- app/api/routes/likes.py
+++ app/api/routes/likes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.api.dependencies import database
-from app.crud.crud_likes import create_like
+from app.crud.crud_likes import create_like, delete_like
 from app.models.domain import users
 from app.services.user_manager import current_active_user
 from app.crud.crud_users import get_user_by_email
@@ -28,6 +28,26 @@ def like_post(
         return {"message": "Like added succcessfully"}
     else:
         raise HTTPException(
-            status_cod=500,
+            status_code=500,
             detail="like create failed",
+        )
+
+
+@router.delete("/post/{post_id}", description="게시글에 관심 표시를 취소합니다.")
+def unlike_post(
+    *,
+    db: Session = Depends(database.get_db),
+    current_user: User = Depends(current_active_user),
+    post_id: int,
+) -> Any:
+    current_user_id = get_user_by_email(db=db, email=current_user.email).id
+
+    deleted_like = delete_like(db=db, post_id=post_id, user_id=current_user_id)
+
+    if deleted_like:
+        return {"message": "Like deleted successfully"}
+    else:
+        raise HTTPException(
+            status_code=500,
+            detail="like delete failed",
         )
