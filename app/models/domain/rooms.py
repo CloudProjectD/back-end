4f8f97f1f61b8b8a34a70154d1eecb479b244c1f
Eeap <kdg97811@naver.com>
2023-12-01 21:12:04 +0900

    feat: add room api & model

--- app/models/domain/rooms.py
+++ app/models/domain/rooms.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 import datetime
 from app.resources.status import Status
@@ -12,3 +12,25 @@ class RoomCreate(BaseModel):
     content: str
     category: str
     status: Status
+
+
+class RoomGet(BaseModel):
+    post_id: int
+    deposit: int
+    price: int
+    title: str
+    content: str
+    status: Status
+    category: str
+    user_id: int
+    created_at: datetime.datetime
+    image_list: List[str]
+
+
+class RoomUpdate(BaseModel):
+    deposit: int
+    price: int
+    title: str
+    content: str
+    status: Status
+    category: str
