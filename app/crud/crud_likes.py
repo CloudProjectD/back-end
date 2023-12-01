779c5ab285cb742cabf51d949c683b7e5dd0bfcd
nana <knh4769@khu.ac.kr>
2023-12-01 16:10:29 +0900

    feat: add like delete function

--- app/crud/crud_likes.py
+++ app/crud/crud_likes.py
@@ -17,3 +17,18 @@ def create_like(db: Session, *, post_id: int, user_id: int) -> Like:
     db.refresh(like)
 
     return like
+
+
+def delete_like(db: Session, *, post_id: int, user_id: int) -> Like:
+    # Check if the like exists
+    like = (
+        db.query(Like).filter(Like.post_id == post_id, Like.user_id == user_id).first()
+    )
+    if not like:
+        raise HTTPException(status_code=404, detail="Like not found")
+
+    # Delete the like
+    db.delete(like)
+    db.commit()
+
+    return like
